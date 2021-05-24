e2ba445cd07e3a4e1b57baf0fc6f016bc5532fdc
Evgeniy Gidzinskiy <50-G-Unit@mail.ru>
2021-05-25 01:09:45 +0700

    feat: Solved all 5 exercises (binary tree most high level difficult

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,55 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
+    if (monthNumber > 0 && monthNumber <= 12) {
+//        NSDateFormatter *dateFormat = [NSDateFormatter new];
+//        NSString *monthFullName = [dateFormat.monthSymbols objectAtIndex: monthNumber - 1];
+//        return monthFullName;
+//    }
+//        NSArray *months = @[@"January", @"February", @"March", @"April", @"May", @"June", @"July", @"August", @"September", @"October", @"November", @"December"];
+//        return months[monthNumber - 1];
+//    }
+        
+    
+        return [NSDateFormatter new].monthSymbols[monthNumber - 1];
+    }
     return nil;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    
+    NSDateFormatter *formatOfDate = [NSDateFormatter new];
+    formatOfDate.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    return [calendar component: NSCalendarUnitDay fromDate: [formatOfDate dateFromString: date]];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSDateFormatter *customDateFormat = [NSDateFormatter new];
+    customDateFormat.locale = [[NSLocale alloc] initWithLocaleIdentifier: @"ru_RU"];
+    customDateFormat.dateFormat = @"E";
+    NSLog(@"%@", [customDateFormat stringFromDate: date] );
+    return [customDateFormat stringFromDate: date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger dateOfWeek = [calendar component: NSCalendarUnitWeekOfYear fromDate: date];
+    NSInteger currWeek = [calendar component: NSCalendarUnitWeekOfYear fromDate: [NSDate now]];
+    if (dateOfWeek == currWeek) {
+        return YES;
+    } else {
+        return NO;
+    }
+//    return NO;
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,32 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    if (ratingArray.count < 3) {
+        return 0;
+    }
+    
+    NSInteger countation = 0;
+    NSInteger n = ratingArray.count;
+    BOOL iLessJ, jLessK, iMoreJ, jMoreK, ascendRate, descenRate;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++) {
+            for (int k = j+1; k < n; k++) {
+                
+                iLessJ = ratingArray[i].intValue < ratingArray[j].intValue;
+                jLessK = ratingArray[j].intValue < ratingArray[k].intValue;
+                ascendRate = iLessJ && jLessK;
+                
+                iMoreJ = ratingArray[i].intValue > ratingArray[j].intValue;
+                jMoreK = ratingArray[j].intValue > ratingArray[k].intValue;
+                descenRate = iMoreJ && jMoreK;
+                
+                if (ascendRate || descenRate) { countation++; }
+            }
+        }
+    }
+    
+    return countation;
 }
 
 @end
